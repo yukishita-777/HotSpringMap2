16d2073ea4978e0d853c74dcfbbcab773fbe60e1
雪下　佳太 <yukishita@mba.local>
2021-03-17 22:55:25 +0900

    application.rbのrspecジェネレーターを訂正

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ module HotSpringMap
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    config.generators do
+    config.generators do |g|
       g.test_framework :rspec,
             view_specs: false,
             helper_specs: false,
